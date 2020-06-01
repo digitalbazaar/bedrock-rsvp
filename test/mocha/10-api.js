3696b687420e39e9e0dc319ca05ae5c33adbde12
Matthew Collier <mcollier@digitalbazaar.com>
2020-06-01 14:57:14 -0400

    Add core files.

---
+++ test/mocha/10-api.js
@@ -0,0 +1,14 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+// const brRsvp = require('bedrock-rsvp');
+
+describe('RSVP Node API', () => {
+  describe('createRequest API', () => {
+    it.skip('creates a request', async () => {
+      // await brRsvp.createRequest();
+    });
+  });
+});
---
+++ test/test.config.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {config} = require('bedrock');
+const path = require('path');
+
+config.mocha.tests.push(path.join(__dirname, 'mocha'));
+
+// mongodb config
+config.mongodb.name = 'bedrock_rsvp_test';
+config.mongodb.host = 'localhost';
+config.mongodb.port = 27017;
+// drop all collections on initialization
+config.mongodb.dropCollections = {};
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
+
+config['https-agent'].rejectUnauthorized = false;
